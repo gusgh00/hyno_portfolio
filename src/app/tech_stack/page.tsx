ffac68831b0ecb0dc3473a9540f86e016ec53a23
HYNOBYTE <hyno0913@naver.com>
2025-08-14 20:32:44 +0900

    swiper js

--- src/app/tech_stack/page.tsx
+++ src/app/tech_stack/page.tsx
@@ -1,10 +1,21 @@
+"use client"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Mousewheel } from 'swiper/modules';
 import styles from "./page.module.css";
 import TechStack01 from "@components/pages/tech_stack/TechStack01";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-      <TechStack01/>
+      <Swiper
+        direction={'vertical'}
+        mousewheel={true}
+        spaceBetween={30}
+        slidesPerView={1}
+        modules={[Mousewheel]}
+      >
+        <SwiperSlide><TechStack01 /></SwiperSlide>
+      </Swiper>
     </div>
   );
 }
