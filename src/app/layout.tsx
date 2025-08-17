2bdddc088aecec8b89c05ce9723b079838ec0ca1
HYNOBYTE <hyno0913@naver.com>
2025-08-17 17:34:09 +0900

    translate no

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" translate="no">
       <body>
         <Header/>
         {children}
