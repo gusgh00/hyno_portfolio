16ed927dc376d89a4d0f05186e58e5785ee1a468
HYNOBYTE <hyno0913@naver.com>
2025-08-19 19:19:30 +0900

    mobile tablet

--- src/_components/header/Header.tsx
+++ src/_components/header/Header.tsx
@@ -3,14 +3,17 @@ import { navigationList } from "@items/NavigationList";
 import LogoDark from "@imgs/logo_dark.png";
 import LogoLight from "@imgs/logo_light.png";
 import styles from "./comp.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import useSchemeStore from "@store/schemeStore";
+import ActiveSelect from "@components/active/ActiveSelect";
 
 const Header = () => {
   const router = useRouter()
-  const {isSchemeType, setSchemeType} = useSchemeStore()
+  const pathname = usePathname()
+  const { isSchemeType, setSchemeType } = useSchemeStore()
+  const [isMenuSelect, setMenuSelect] = useState<number>(0)
 
   useEffect(() => {
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -27,16 +30,26 @@ const Header = () => {
       document.documentElement.setAttribute('color-theme', 'light');
     }
   }, [isSchemeType])
+
+  useEffect(() => {
+    const url = navigationList.filter((item, index) => index === isMenuSelect)[0].url
+    router.push(url)
+  }, [isMenuSelect])
+
+  useEffect(() => {
+    const menuIndex = navigationList.findIndex((item, index) => item.url === pathname)
+    setMenuSelect(menuIndex)
+  }, [pathname])
   return (
     <>
       <header className={styles.header}>
         <div className={styles.header_main}>
           <div className={styles.header_title} onClick={() => router.push("/")}>
             {!isSchemeType
-            ?
-            <Image src={LogoLight} alt="logo_light" className={styles.logo}/>
-            :
-            <Image src={LogoDark} alt="logo_dark" className={styles.logo}/>
+              ?
+              <Image src={LogoLight} alt="logo_light" className={styles.logo} />
+              :
+              <Image src={LogoDark} alt="logo_dark" className={styles.logo} />
             }
           </div>
           <div className={styles.navi_section}>
@@ -45,6 +58,15 @@ const Header = () => {
                 {item.name}
               </div>
             ))}
+            <div className={styles.navi_select}>
+              <ActiveSelect
+                onSelect={(index: number) => setMenuSelect(index)}
+                disabled={false}
+                selected_value={isMenuSelect}
+                select_list={navigationList.map(item => item.name)}
+                placeholder="메뉴 선택"
+              />
+            </div>
           </div>
         </div>
       </header>
